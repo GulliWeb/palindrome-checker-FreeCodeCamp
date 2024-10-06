e07baba7817eb7a018b9cee3f74fe18356326966
Andrea Gulli <andreagulli11@gmail.com>
2024-10-06 23:33:12 +0200

    add alphanumeric numbers check

--- script.js
+++ script.js
@@ -6,28 +6,29 @@ const txtCheck = document.getElementById('text-input')
 const btnResult = document.getElementById('result')
 
 let word 
-// Creo una funzione che controlla se ho inserito un valore nella textBox e se la frase ricevuta in input è palindroma una volta cliccato il bottone check. -->
 
+// Creo una funzione che controlla se ho inserito un valore nella textBox e se la frase ricevuta in input è palindroma una volta cliccato il bottone check. -->
 const isPalindroma = () =>{
     word = txtCheck.value
-    let palindroma = ""
 
-  if (word == "") {
-    alert("Please input a value")
-    return 
-  }  else{
-    // Controllo se la frase è palindroma  
-    for(let i = word.length -1; i >= 0; i--){
-        palindroma = palindroma + word[i]
+    if (word == "") {
+      alert("Please input a value")
+      return 
+    }  else{
+      // Rimuovo tutti i caratteri non alfanumerici e converto tutto in lowercase
+      let cleanedWord = word.toLowerCase().replace(/[^a-z0-9]/gi, '')
+
+      // Creo la parola invertita per verificare se è palindroma
+      let reversedWord = cleanedWord.split('').reverse().join('')
+
+      // Confronto la parola invertita con quella pulita
+      if(cleanedWord === reversedWord){
+          btnResult.innerText = `${word} is a palindrome`
+      } else{
+          btnResult.innerText = `${word} is not a palindrome.`
+      }
     }
-    if(palindroma === word){
-        btnResult.innerText = `${word} is a palindrome`
-    } else{
-        btnResult.innerText = `${word} is not a palindrome.`
-    }
-  }
 }
 
 // Richiamo la funzione al click sul bottone
 btnCheck.addEventListener('click', isPalindroma)
-
