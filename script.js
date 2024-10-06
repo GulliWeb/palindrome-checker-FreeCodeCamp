595f48181ae23d647ca92fa07f018adc15135536
Andrea Gulli <andreagulli11@gmail.com>
2024-10-06 22:29:38 +0200

    complete JS logic

---
+++ script.js
@@ -0,0 +1,33 @@
+// Raccolgo i valori dal DOM
+const btnCheck = document.getElementById('check-btn')
+
+const txtCheck = document.getElementById('text-input')
+
+const btnResult = document.getElementById('result')
+
+let word 
+// Creo una funzione che controlla se ho inserito un valore nella textBox e se la frase ricevuta in input è palindroma una volta cliccato il bottone check. -->
+
+const isPalindroma = () =>{
+    word = txtCheck.value
+    let palindroma = ""
+
+  if (word == "") {
+    alert("Please input a value")
+    return 
+  }  else{
+    // Controllo se la frase è palindroma  
+    for(let i = word.length -1; i >= 0; i--){
+        palindroma = palindroma + word[i]
+    }
+    if(palindroma === word){
+        console.log(`La parola ${word} è palindroma`)
+    } else{
+        console.log(`La parola ${word} non è palindroma`)
+    }
+  }
+}
+
+// Richiamo la funzione al click sul bottone
+btnCheck.addEventListener('click', isPalindroma)
+
